20718f39cf6de396456de1e0d4f445ef8cf66746
marsman <marsman@26f514af-f824-0410-8d19-d9195fddda1e>
2007-01-05 12:43:02 +0000

    parameters can also be dummy, just for simple testing or use without eyelink etc.

--- Percept/SaliencyExperiment/flip.m
+++ Percept/SaliencyExperiment/flip.m
@@ -4,16 +4,20 @@ function[new_parameters] = flip( window, parameters, description )
     end;
     c = clock;
     Screen('Flip', window);
-    log('Screen Flip', parameters(6).value);
-    flips = parameters(4).value;
-    descriptions = parameters(5).value;
     
-    s = size(flips, 1);
-    flips(s+1,:) = c;
+    if (strcmp(class(parameters), 'struct') == 1)
+        log('Screen Flip', parameters(6).value);
+        flips = parameters(4).value;
+        descriptions = parameters(5).value;
+        
+        s = size(flips, 1);
+        flips(s+1,:) = c;
  
-    descriptions{s+1} = description;
-    parameters(4).value = flips;
-    parameters(5).value = descriptions;
+        descriptions{s+1} = description;
+        parameters(4).value = flips;
+        parameters(5).value = descriptions;
     
-    new_parameters = parameters;
-    
+        new_parameters = parameters;
+    else
+        new_parameters = -1;
+    end;
